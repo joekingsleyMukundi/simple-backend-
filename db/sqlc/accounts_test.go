63e8c67445c3c08586e84122f9f99996043e39b3
joekingsleyMukundi <joekingsleymukundi@gmail.com>
2023-05-09 23:55:14 +0300

    create the database infrastructure and write test for queries also included transactions to ensure ACID properties are met

---
+++ db/sqlc/accounts_test.go
@@ -0,0 +1,93 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/joekingsleyMukundi/banker/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomAccount(t *testing.T) Account {
+	args := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+	account, err := testQueries.CreateAccount(context.Background(), args)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	require.Equal(t, args.Owner, account.Owner)
+	require.Equal(t, args.Balance, account.Balance)
+	require.Equal(t, args.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+	return account
+}
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	account1, err := testQueries.GetAccount(context.Background(), account.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	require.Equal(t, account.Owner, account1.Owner)
+	require.Equal(t, account.Balance, account1.Balance)
+	require.Equal(t, account.Currency, account1.Currency)
+	require.WithinDuration(t, account.CreatedAt, account1.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+	account1, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account1)
+
+	require.NotZero(t, account1.ID)
+	require.NotZero(t, account1.CreatedAt)
+	require.Equal(t, arg.Balance, account1.Balance)
+	require.Equal(t, account.Currency, account1.Currency)
+	require.Equal(t, account.ID, account1.ID)
+	require.Equal(t, account.CreatedAt, account1.CreatedAt)
+	require.Equal(t, account.Owner, account1.Owner)
+	require.WithinDuration(t, account.CreatedAt, account1.CreatedAt, time.Second)
+}
+func TestDeleteAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	account1, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account1)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	args := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), args)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
---
+++ db/sqlc/store.go
@@ -0,0 +1,148 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// store provides all functions to execute db queries and transctions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// New store creates a new store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes a function within a database transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+// transfertxparams contains the inputparameters of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_amount_id"`
+	ToAccountID   int64 `json:"to_accoint_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// Transferresultt s the result of the transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+var txKey = struct{}{}
+
+// TransferTx perfoms a money transfer from one account to the other.
+// it creates a transfer record, add account entries, and update accounts` within a single database transaction
+func (store *Store) TransferTx(
+	ctx context.Context,
+	arg TransferTxParams,
+) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		txName := ctx.Value(txKey)
+
+		fmt.Println(txName, "create transfer")
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "create entry 1")
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "create entry 2")
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// get account -> upate accounts balance
+
+		/*fmt.Println(txName, "get account1")
+		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+		if err != nil {
+			return err
+		}*/
+
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(
+				ctx,
+				q,
+				arg.FromAccountID,
+				arg.ToAccountID,
+				-arg.Amount,
+				arg.Amount,
+			)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.FromAccountID, arg.Amount, -arg.Amount)
+		}
+		return nil
+	})
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	accountID2 int64,
+	amount1 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+	account2, err = q.UpdateAccountBalance(ctx, UpdateAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	if err != nil {
+		return
+	}
+	return
+}
